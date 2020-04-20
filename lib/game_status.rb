9cb0235ec003fbdd532193aca91c24a6e45e1bd9
Jacob Gonchar <jake098877@gmail.com>
2020-04-20 23:15:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,66 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return false
+  end
+  i = 0
+  WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+  if position_1 == ("X") && position_2 == ("X") && position_3 == ("X") || position_1 == ("O") && position_2 == ("O") && position_3 == ("O")
+    return win_combination
+  else
+     i += 1
+  end
+
+end
+if i = 8
+  return false
+end
+end
+
+def full?(board)
+  if won?(board)
+    return true
+  end
+
+    board.all? do |index|
+      index == "X" || "O"
+      return  true
+      board.detect{|i| i.(" ")}
+        return false
+
+  end
+
+end
+
+def draw?(board)
+  if win_combination(board)
+    return false
+    board.detect{|i| i.(" ")}
+      return false
+    end
+    board.all? do |index|
+      index == "X" || "O"
+        return true
+      end
+  end
