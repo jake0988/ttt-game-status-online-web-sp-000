19ad7a9d9105ed38cfd32a6e8678157841f24e18
Jacob Gonchar <jake098877@gmail.com>
2020-04-21 00:31:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,28 +20,28 @@ def won?(board)
     return false
   end
 
-#   i = 0
-#   WIN_COMBINATIONS.each do |win_combination|
-#       win_index_1 = win_combination[0]
-#       win_index_2 = win_combination[1]
-#       win_index_3 = win_combination[2]
-#
-#       position_1 = board[win_index_1]
-#       position_2 = board[win_index_2]
-#       position_3 = board[win_index_3]
-#
-#   if position_1 == ("X") && position_2 == ("X") && position_3 == ("X") || position_1 == ("O") && position_2 == ("O") && position_3 == ("O")
-#     return win_combination
-#   else
-#      i += 1
-#   end
-#
-#
-# if i == 8
-#   return false
-# end
-# end
-#
+  i = 0
+  WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+  if position_1 == ("X") && position_2 == ("X") && position_3 == ("X") || position_1 == ("O") && position_2 == ("O") && position_3 == ("O")
+    return win_combination
+  else
+     i += 1
+  end
+
+
+if i == 8
+  return false
+end
+end
+
 # def full?(board)
 #   if won?(board)
 #     return true
